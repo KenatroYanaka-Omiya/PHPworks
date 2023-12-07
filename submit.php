4973e90cd1a199f80daa32b24857a13447fa9744
KenatroYanaka-Omiya <145731910+KenatroYanaka-Omiya@users.noreply.github.com>
2023-12-07 11:24:41 +0900

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>送信完了</title>
+</head>
+<body>
+    <!-- Head[Start] -->
+    <header>
+    </header>
+    <!-- Head[End] -->
+
+    <!-- Main[Start] -->
+    <h1>送信完了しました</h1>
+    <div class="buttons">
+        <div class="return_button">
+            <a href="index.php">戻る
+        </div>
+        <div class="admin_button">
+            <a href="admin.php">管理画面へ
+        </div>
+    </div>
+    
+<!-- Main[End] -->
+</body>
+</html>
