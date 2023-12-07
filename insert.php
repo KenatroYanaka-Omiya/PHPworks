69299a806762c7c8ed56852f5ef63804ace50a91
KenatroYanaka-Omiya <145731910+KenatroYanaka-Omiya@users.noreply.github.com>
2023-12-07 11:23:10 +0900

    12/7 insert.php
    ファイル名をconfig.phpからinsert.phpに変更

--- insert.php
+++ insert.php
@@ -1,4 +1,6 @@
 <?php
+
+
 //1. POSTデータ取得
 $name   = $_POST["name"];
 $area  = $_POST["area"];
@@ -6,25 +8,28 @@ $age    = $_POST["age"];
 $know_from = $_POST["know_from"];
 $comment = $_POST["comment"];
 
+$checkbox = implode(",",$know_from);
+
 //2. DB接続します
 include "functions.php";
 $pdo = db_con();
 
 
 //３．データ登録SQL作成
-$sql = "INSERT INTO post(name,area,age,know_from,comment,created_at)VALUES(:name,:area,:age,:know_from,:comment,sysdate())";
+$sql = "INSERT INTO posts2(name,area,age,know_from,comment,created_at)VALUES(:name,:area,:age,:know_from,:comment,sysdate())";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->bindValue(':area', $area, PDO::PARAM_STR);   
 $stmt->bindValue(':age', $age, PDO::PARAM_STR);
-$stmt->bindValue(':know_from', $know_from, PDO::PARAM_STR);
-$stmt->bindValue(':commnet', $comment, PDO::PARAM_STR);
+$stmt->bindValue(':know_from', $checkbox, PDO::PARAM_STR);
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
 $status = $stmt->execute();
 if ($status == false) {
     sqlError($stmt);
     $error = $stmt->errorInfo();
     exit("ErrorSQL:".$error[2]);
 } else {
+    echo("送信しました");
     redirect("submit.php");
     exit;
 }
